677b26ed725ad6a4f5103fc0d1a72fc5056e0225
Guilherme Mierzwa <guilherme.mierzwa@tapps.com.br>
2018-07-04 15:54:57 -0300

    Add missing nyc dependency and fix test file

--- test/common.js
+++ test/common.js
@@ -2,7 +2,10 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const config = require('../config');
+const fs = require('fs');
 chai.use(chaiHttp);
+chai.should();
 
 function successChecks(err, res, status) {
     if (res.statusCode !== status) {
@@ -27,7 +30,77 @@ function errorChecks(err, res, status) {
     }
 }
 
+function importTest(name, path) {
+    describe(name, function() {
+        require(path);
+    });
+}
+
+function deleteLogs(done) {
+    const path = '/var/log/app_engine/custom_logs/';
+    fs.readdir(path, (error, files) => {
+        if (error) {
+            done(error);
+        }
+        const fileBaseName = `app-${config.service_name}-`;
+        const matcher = new RegExp(fileBaseName + '.*');
+        const matchedFiles = files.filter(name => matcher.test(name));
+        matchedFiles.forEach(filename => {
+            fs.unlink(path + filename, error => {
+                done(error);
+            });
+        });
+        done();
+    });
+}
+
+function assertDatastoreKey(datastore, object) {
+    return datastore.read(object)
+        .catch(error => {
+            if (process.env.DATASTORE_EMULATOR_HOST) {
+                const uuid = require('uuid/v4');
+                object.data = {
+                    key: uuid(),
+                };
+                return datastore.write(object);
+            } else {
+                throw error;
+            }
+        });
+}
+
+function systemTestSetup(config, done) {
+    global.baseHeaders = {
+        'content-type': 'application/json',
+    };
+    const tpCommon = require('tp-common');
+    const datastore = new tpCommon.Datastore(config);
+    const promises = [];
+    const serviceAccountKey = {
+        id: 'adminkey',
+        kind: 'ServiceAccountKeys',
+        namespace: config.service_name,
+    };
+    const sharedCloudSecret = {
+        id: 'latest',
+        kind: 'SharedCloudSecret',
+        namespace: 'cloud-configs',
+    };
+    promises.push(assertDatastoreKey(datastore, serviceAccountKey));
+    promises.push(assertDatastoreKey(datastore, sharedCloudSecret));
+    Promise.all(promises).then(([serviceAccountKey, sharedCloudSecret]) => {
+        global.baseHeaders['x-tapps-service-account-key'] = serviceAccountKey;
+        global.serviceAccountKey = serviceAccountKey;
+        global.baseHeaders['x-tapps-shared-cloud-secret'] = sharedCloudSecret;
+        global.sharedCloudSecret = sharedCloudSecret;
+        done();
+    }).catch(done);
+}
+
 module.exports = {
+    deleteLogs,
     errorChecks,
+    importTest,
     successChecks,
+    systemTestSetup,
 };
--- test/test.js
+++ test/test.js
@@ -1,92 +1,29 @@
 'use strict';
 
-const fs = require('fs');
-
+const common = require('./common');
 const config = require('../config');
+
+process.on('unhandledRejection', ex => {
+    throw ex;
+});
 
 global.baseHeaders = {
     'content-type': 'application/json',
     'x-tapps-bundle-id': 'test.bundle.id',
 };
 
-process.on('unhandledRejection', ex => {
-    throw ex;
+describe('Unit tests', function() {
+    // common.importTest('Test name', './unit/test-file');
+});
+describe('Integration tests', function() {
+    // common.importTest('Test name', './integration/test-file');
+});
+describe('System tests', function() {
+    before(function(done) {
+        common.systemTestSetup(config, done);
+    });
+    // common.importTest('Test name', './system/test-file');
+    common.importTest('Health Check', './system/health-check');
 });
 
-function importTest (name, path) {
-    describe(name, function() {
-        require(path);
-    });
-}
-
-function systemTestSetup (done) {
-    // Read the Shared Cloud Secret from Datastore
-    const tpCommon = require('tp-common');
-    const datastore = new tpCommon.Datastore(config);
-    datastore.read({
-        id: 'latest',
-        kind: 'SharedCloudSecret',
-        namespace: 'cloud-configs',
-    }).then(result => {
-        global.baseHeaders['x-tapps-shared-cloud-secret'] = result.key;
-        done();
-    }).catch(error => {
-        if (process.env.DATASTORE_EMULATOR_HOST) {
-            const uuid = require('uuid/v4');
-            const key = uuid();
-            let date = new Date().getTime();
-            date += 60 * 60 * 1000;
-            datastore.write({
-                id: 'latest',
-                kind: 'SharedCloudSecret',
-                namespace: 'cloud-configs',
-                data: {
-                    expiration: date,
-                    key: key,
-                },
-            }).then(function() {
-                global.baseHeaders['x-tapps-shared-cloud-secret'] = key;
-                done();
-            }).catch(error => {
-                throw error;
-            });
-        } else {
-            throw error;
-        }
-    });
-}
-
-describe('Service Name Tests', function() {
-    describe('Unit tests', function() {
-        // importTest('Test name', './unit/test-file');
-        importTest('Datastore Instrumentation', './unit/datastore-instrumentation');
-    });
-    describe('Integration tests', function() {
-        // importTest('Test name', './integration/test-file');
-    });
-    describe('System tests', function() {
-        before(function(done) {
-            systemTestSetup(done);
-        });
-        // importTest('Test name', './system/test-file');
-        importTest('Health Check', './system/health-check');
-    });
-    after(function(done) {
-        // Remove any leftover logs
-        const path = '/var/log/app_engine/custom_logs/';
-        fs.readdir(path, (error, files) => {
-            if (error) {
-                done(error);
-            }
-            const fileBaseName = `app-${config.service_name}-`;
-            const matcher = new RegExp(fileBaseName + '.*');
-            const matchedFiles = files.filter(name => matcher.test(name));
-            matchedFiles.forEach(filename => {
-                fs.unlink(path + filename, error => {
-                    done(error);
-                });
-            });
-            done();
-        });
-    });
-});
+after(common.deleteLogs);
