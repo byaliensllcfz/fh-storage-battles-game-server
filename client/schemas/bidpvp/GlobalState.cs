b5c0daed59621ea36a4242b2fea9c2facbbe8226
Caio Teixeira <caio.teixeira@tapps.com.br>
2019-10-17 11:24:16 -0300

    Update Client schemas

--- client/schemas/bidpvp/GlobalState.cs
+++ client/schemas/bidpvp/GlobalState.cs
@@ -1,6 +1,20 @@
-using Plugins.Colyseus.Serializer.Schema;
+// 
+// THIS FILE HAS BEEN GENERATED AUTOMATICALLY
+// DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
+// 
+// GENERATED USING @colyseus/schema 0.4.61
+// 
+
+using Colyseus.Schema;
 
 public class GlobalState : Schema {
 	[Type(0, "string")]
 	public string status = "";
+
+	[Type(1, "ref", typeof(AuctionState))]
+	public AuctionState auction = new AuctionState();
+
+	[Type(2, "map", typeof(MapSchema<PlayerState>))]
+	public MapSchema<PlayerState> players = new MapSchema<PlayerState>();
 }
+
---
+++ client/schemas/bidpvp/PlayerState.cs
@@ -0,0 +1,17 @@
+// 
+// THIS FILE HAS BEEN GENERATED AUTOMATICALLY
+// DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
+// 
+// GENERATED USING @colyseus/schema 0.4.61
+// 
+
+using Colyseus.Schema;
+
+public class PlayerState : Schema {
+	[Type(0, "string")]
+	public string id = "";
+
+	[Type(1, "int32")]
+	public int money = 0;
+}
+
