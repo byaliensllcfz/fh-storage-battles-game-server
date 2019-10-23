647394c22f6906dafd0f67d1af0fd9bb7047fc0e
Renato Pereira <renato.ppontes@gmail.com>
2019-10-23 19:34:38 -0300

    wip

--- src/rooms/bidpvp/controllers/auction-controller.js
+++ src/rooms/bidpvp/controllers/auction-controller.js
@@ -4,36 +4,30 @@ const lodash = require('lodash');
 const {MapSchema} = require('@colyseus/schema');
 
 const configHelper = require('../../../helpers/config-helper');
+const profileDao = require('../../../daos/profile-dao');
 
 class AuctionController {
     constructor(room) {
         this.room = room;
         this.state = room.state;
         this.auctionEndTimeout = null;
-        
+
         this.configs = configHelper.get();
     }
 
-    drawItems(itemAmount){
-
-        let itemsStart = new MapSchema();
-        let playableItems = this.configs.items;
-        for (let i = 0; i < itemAmount; i++) {
-            itemsStart[i] = lodash.sample(playableItems).id;
-        }
-        this.state.auction.items = itemsStart;
-    }
-
-    startAuction() {
+    async startAuction() {
         lodash.each(this.state.players, (player) => {
             player.money = 1000 + Math.floor(Math.random()*1000);
         });
+        
+        // const ids = this.state.players.map(p => p.id);
+        // const profiles = await profileDao.getProfiles(ids);
 
         this.state.auction.bidValue = this.configs.game.bidIncrement;
         this.state.status = 'PLAY';
 
         this.auctionEndTimeout = this.room.clock.setTimeout(() => this._runDole(), this.configs.game.auctionInitialDuration);
-        this.drawItems(lodash.random(5,8));
+        this._drawItems(lodash.random(5,8));
     }
 
     bid(playerId) {
@@ -53,6 +47,15 @@ class AuctionController {
             this.auctionEndTimeout.clear();
         }
         this.auctionEndTimeout = this.room.clock.setTimeout(() => this._runDole(), this.configs.game.auctionAfterBidDuration);
+    }
+
+    _drawItems(itemAmount){
+        let itemsStart = new MapSchema();
+        let playableItems = this.configs.items;
+        for (let i = 0; i < itemAmount; i++) {
+            itemsStart[i] = lodash.sample(playableItems).id;
+        }
+        this.state.auction.items = itemsStart;
     }
 
     _runDole() {
