c1b8d78f4cf44be6fe0b8bd06a9b24b28a003fe7
Guilherme Mierzwa <guilherme.mierzwa@tapps.com.br>
2019-12-09 14:31:29 -0300

    Remove console.log

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@
 // The Datadog require has to be the first thing to run!
 require('@tapps-games/datadog');
 
-process.on('unhandledRejection', error => {
-    console.log('unhandledRejection', error);
-});
-
 (async function () {
     const { config } = require('@tapps-games/core');
     await config.load('env');
     await config.load('json', 'config.json');
+
+    const { Logger } = require('@tapps-games/logging');
+    const logger = new Logger();
+    process.on('unhandledRejection', error => {
+        logger.error(`Unhandled Promise Rejection: ${error.message}`, error);
+    });
 
     const { createServer } = require('./server');
     await createServer();
